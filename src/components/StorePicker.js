f2afe528c30cf53f4819923c4ece381b179e524f
Ariel Blasi <exorcismo@gmail.com>
2018-11-21 17:50:09 -0300

    console log input ref

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 export default function StorePicker() {
+	const myInput = React.createRef();
+
+	const goToStore = (e) => {
+		e.preventDefault()
+		//grab store name
+		console.log(myInput)
+		console.log(myInput.current.value)
+		//go to store/storename
+	}
+
 	return (
-		<>
-			<form className="store-selector">
-				<h2>Please Enter A Store</h2>
-				<input type="text" required placeholder='Store Name' />
-				<button type="submit">Visit Store →</button>
-			</form>
-		</>
+		<form className="store-selector" onSubmit={goToStore}>
+			<h2>Please Enter A Store</h2>
+			<input ref={myInput} type="text" required placeholder='Store Name' defaultValue={getFunName()} />
+			<button type="submit">Visit Store →</button>
+		</form>
 	)
 }
