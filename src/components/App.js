3aaebf1d7951d1becf88eee33f7d5ce763ca4465
litesoul <exorcismo@gmail.com>
2018-11-21 13:55:07 -0300

    🏭

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Order from './Order'
+import Inventory from './Inventory'
+
+export default function App() {
+	return (
+		<div className="catch-of-the-day">
+			<div className="menu">
+				<Header></Header>
+			</div>
+			<Order></Order>
+			<Inventory></Inventory>
+		</div>
+	)
+}
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Header() {
+	return (
+		<header className="top">
+			<h1>Catch
+				<span className="ofThe">
+					<span className="of">Of</span>
+					<span className="the">The</span>
+				</span>
+				Day</h1>
+			<h3 className="tagline">
+				<span>Fresh Daily</span>
+			</h3>
+		</header>
+	)
+}
