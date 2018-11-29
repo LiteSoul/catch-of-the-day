576e19de155126240561e3654b1e3660dc7ec0c1
Ariel Blasi <exorcismo@gmail.com>
2018-11-29 18:31:07 -0300

    edit inventory

--- src/components/App.js
+++ src/components/App.js
@@ -1,86 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import Header from './Header'
-import Order from './Order'
-import Inventory from './Inventory'
-import Fish from './Fish'
-import sampleFishesBody from '../samples-fishes-body'
+import React, { useState, useEffect } from "react";
+import Header from "./Header";
+import Order from "./Order";
+import Inventory from "./Inventory";
+import Fish from "./Fish";
+import sampleFishesBody from "../samples-fishes-body";
 // import base from '../base'
-import firebase from '../firebase'
+import firebase from "../firebase";
 // import { useList, useObject } from 'react-firebase-hooks/database';
 
-export default function App({ match: { params: { storeId } } }) {
-	const [fishes, setFish] = useState({})
-	const [order, setOrder] = useState(JSON.parse(localStorage.getItem(storeId)) || {})
+export default function App({
+  match: {
+    params: { storeId }
+  }
+}) {
+  const [fishes, setFish] = useState({});
+  const [order, setOrder] = useState(
+    JSON.parse(localStorage.getItem(storeId)) || {}
+  );
 
-	const fishesRef = firebase.database().ref(`${storeId}/fishes/`)
+  const fishesRef = firebase.database().ref(`${storeId}/fishes/`);
 
-	useEffect(() => {
-		//Take a snapshot of the DB
-		fishesRef.on('value', (snapshot) => {
-			console.log('Snapshot of the database:')
-			console.log(snapshot.val())
-			// setFish(prevState => { return { ...snapshot.val(), ...prevState } })
-			setFish(snapshot.val())
-		})
+  useEffect(() => {
+    //Take a snapshot of the DB
+    fishesRef.on("value", snapshot => {
+      console.log("Snapshot of the database:");
+      console.log(snapshot.val());
+      // setFish(prevState => { return { ...snapshot.val(), ...prevState } })
 
-		return () => {
-			//Stop the listener
-			fishesRef.off()
-		}
-	}, [])
+      //populate state with db at initial render
+      setFish(snapshot.val());
+    });
 
-	const addFish = (fish) => {
-		const newFishKey = firebase.database().ref(storeId).child('fishes').push().key;
-		let fishObject = {};
-		fishObject[newFishKey] = fish;
-		fishesRef.update(fishObject);
-		// setFish(prevState => { return { ...fishObject, ...prevState } })
-		console.log('fishes state from addFish (missing latest):')
-		console.log(fishes)
-	}
+    return () => {
+      //Stop the listener
+      fishesRef.off();
+    };
+  }, []);
 
-	const loadSampleFishes = () => {
-		sampleFishesBody.map(fish => addFish(fish))
-	}
+  useEffect(() => {
+    //pass state to db
+    // fishesRef.update(fishes)
+  }, []);
 
-	const addToOrder = (key) => {
-		//copy original order so we can add/update/modify data
-		const newOrder = { ...order }
-		//add a fishID to order, along with quantity
-		newOrder[key] = newOrder[key] + 1 || 1
-		//set it
-		setOrder(newOrder)
-		console.log(order)
-	}
+  const addFish = fish => {
+    const newFishKey = firebase
+      .database()
+      .ref(storeId)
+      .child("fishes")
+      .push().key;
+    let fishObject = {};
+    fishObject[newFishKey] = fish;
+		fishesRef.update(fishObject)
+  };
 
-	useEffect(() => {
-		console.log('SECOND USEEFFECT!')
-		//first reinstate localStorage
-		// const localStorageRef = localStorage.getItem(storeId)
-		// console.log('LOCALSTORAGEREF ' + localStorageRef)
-		// if (localStorageRef) {
-		// 	setOrder(JSON.parse(localStorageRef))
-		// }
-		//save orders to localStorage
-		localStorage.setItem(storeId, JSON.stringify(order))
-	}, [order])
+  const loadSampleFishes = () => {
+    sampleFishesBody.map(fish => addFish(fish));
+  };
 
-	return (
-		<div className="catch-of-the-day">
-			<div className="menu">
-				<Header tagline='Fresh Seafood Market'></Header>
-				<ul className="fishes">
-					{Object.entries(fishes).map(([key, fish]) => <Fish key={key} fishID={key} fish={fish} addToOrder={addToOrder} />)}
-				</ul>
-			</div>
-			<Order
-				fishes={fishes}
-				order={order}
-			></Order>
-			<Inventory
-				addFish={addFish}
-				loadSampleFishes={loadSampleFishes}
-			></Inventory>
-		</div>
-	)
+  const updateFish = updatedFullFish => {
+    //take a copy of  the current state
+    // const fishes= {...fishes}
+    //update that state
+    fishesRef.update(updatedFullFish);
+    console.log(updatedFullFish);
+    //set that to state
+  };
+
+  const addToOrder = key => {
+    //copy original order so we can add/update/modify data
+    const newOrder = { ...order };
+    //add a fishID to order, along with quantity
+    newOrder[key] = newOrder[key] + 1 || 1;
+    //set it
+    setOrder(newOrder);
+    console.log(order);
+  };
+
+  useEffect(
+    () => {
+      localStorage.setItem(storeId, JSON.stringify(order));
+    },
+    [order]
+  );
+
+  return (
+    <div className="catch-of-the-day">
+      <div className="menu">
+        <Header tagline="Fresh Seafood Market" />
+        <ul className="fishes">
+          {fishes &&
+            Object.entries(fishes).map(([key, fish]) => (
+              <Fish
+                key={key}
+                fishID={key}
+                fish={fish}
+                addToOrder={addToOrder}
+              />
+            ))}
+        </ul>
+      </div>
+      <Order fishes={fishes} order={order} />
+      <Inventory
+        addFish={addFish}
+        updateFish={updateFish}
+        loadSampleFishes={loadSampleFishes}
+        fishes={fishes}
+      />
+    </div>
+  );
 }
---
+++ src/components/EditFishForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function EditFishForm({ fish, fishId,updateFish }) {
+  const handleChange = e => {
+    console.log(e.currentTarget.value);
+    //update that fish
+    //1. take a copy of the current fish
+    const updatedFish = {
+      ...fish,
+      [e.currentTarget.name]: e.currentTarget.value
+    };
+		const updatedFullFish = { [fishId]: updatedFish }
+		updateFish(updatedFullFish)
+  };
+
+  return (
+    <div className="fish-edit">
+      <input
+        type="text"
+        name="name"
+        value={fish.name}
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="price"
+        value={fish.price}
+        onChange={handleChange}
+      />
+      <select name="status" value={fish.status} onChange={handleChange}>
+        <option value="available">Fresh!</option>
+        <option value="unavailable">Sold Out!</option>
+      </select>
+      <textarea name="desc" value={fish.desc} onChange={handleChange} />
+      <input
+        type="text"
+        name="image"
+        value={fish.image}
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
