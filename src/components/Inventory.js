d91b297f1847ebfe5828430050d8a473470b5daf
Ariel Blasi <exorcismo@gmail.com>
2018-12-04 18:59:12 -0300

    add login buttons

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import PropTypes from "prop-types"
-import AddFishForm from './AddFishForm'
-import EditFishForm from './EditFishForm'
+import React from "react";
+import PropTypes from "prop-types";
+import AddFishForm from "./AddFishForm";
+import EditFishForm from "./EditFishForm";
+import Login from "./Login";
 
 Inventory.propTypes = {
 	addFish: PropTypes.func,
@@ -9,15 +10,31 @@ Inventory.propTypes = {
 	deleteFish: PropTypes.func,
 	loadSampleFishes: PropTypes.func,
 	fishes: PropTypes.object
-}
+};
 
-export default function Inventory({ addFish, updateFish, deleteFish, loadSampleFishes, fishes }) {
+export default function Inventory({
+	addFish,
+	updateFish,
+	deleteFish,
+	loadSampleFishes,
+	fishes
+}) {
+	return <Login />;
 	return (
 		<div className="inventory">
 			<h2>Inventory</h2>
-			{fishes && Object.entries(fishes).map(([key, fish]) => <EditFishForm key={key} fishId={key} fish={fish} updateFish={updateFish} deleteFish={deleteFish} />)}
-			<AddFishForm addFish={addFish}></AddFishForm>
+			{fishes &&
+				Object.entries(fishes).map(([key, fish]) => (
+					<EditFishForm
+						key={key}
+						fishId={key}
+						fish={fish}
+						updateFish={updateFish}
+						deleteFish={deleteFish}
+					/>
+				))}
+			<AddFishForm addFish={addFish} />
 			<button onClick={loadSampleFishes}>Load Sample Fishes</button>
 		</div>
-	)
+	);
 }
