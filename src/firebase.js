e623c12902eabb42d4feda9b01b5dc3dd6f874df
litesoul <exorcismo@gmail.com>
2018-12-12 14:18:54 -0300

    firebase api

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,11 @@
 import firebase from "firebase";
 const config = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+	// apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	apiKey: "AIzaSyB50Xl-Zzlk3fGxCXqHE74ZI10rT190wCQ",
+	// authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	authDomain: "catch-in-da-night.firebaseapp.com",
+	// databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
+	databaseURL: "https://catch-in-da-night.firebaseio.com"
 };
 const firebaseApp = firebase.initializeApp(config);
 export { firebaseApp };
